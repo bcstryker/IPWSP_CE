725d9c4e53c597fd4bef2d50d4f175698ba77313
bcstryker <bcstryker@gmail.com>
2018-04-25 15:40:11 -0400

    lab 4

---
+++ lab4.py
@@ -0,0 +1,14 @@
+import sys
+import openpyxl
+
+filename = 'home/student/ipwsp/aws/AWS_SEED.xlsx'
+column = 'A'
+row = 3
+
+wb = load_workbook(filename = filename)
+ws = wb.active
+sheet = wb['Sheet1']
+cell_name = column + str(row)
+x = sheet[cell_name].value
+
+print x
