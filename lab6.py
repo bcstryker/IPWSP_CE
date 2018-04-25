4b96e977f28a48957fc0bae3269427702d16b622
bcstryker <bcstryker@gmail.com>
2018-04-25 15:41:12 -0400

    lab 6

---
+++ lab6.py
@@ -0,0 +1,28 @@
+import sys
+import openpyxl
+
+filename = 'home/student/ipwsp/aws/AWS_SEED.xlsx'
+column = 'A'
+row = 3
+
+class AWS_VPC():
+    def __init__(self):
+        try:
+            wb = load_workbook(filename = filename, data_only = True)
+        except IOError:
+            sys.exit('Could not open file')
+        else:
+            self.wb = wb
+
+    def get_row(col, row = 1, max_row = 4):
+        ws = self.wb.active
+        sheet = wb['Sheet1']
+        cell_name = column + str(row)
+        x = sheet[cell_name].value
+        return x
+
+col_A = AWS_VPC()
+col_B = AWS_VPC()
+
+contents_A = col_A.get_row('A')
+contents_B = col_B.get_row('B')
