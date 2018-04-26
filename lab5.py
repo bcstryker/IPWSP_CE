b6e549406ce264215b165fe746cfa822f3ada280
stryker <bcstryker@gmail.com>
2018-04-26 22:09:05 +0000

    lab 5 debugged

--- lab5.py
+++ lab5.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import load_workbook
 
-filename = 'home/student/ipwsp/aws/AWS_SEED.xlsx'
+filename = '/home/student/ipwsp/aws/AWS_SEED.xlsx'
 column = 'A'
 row = 3
 
@@ -9,7 +9,7 @@ def get_row(col, row = 1, max_row = 4):
     wb = load_workbook(filename = filename, data_only = True)
     ws = wb.active
     sheet = wb['Sheet1']
-    cell_name = column + str(row)
+    cell_name = col + str(row)
     x = sheet[cell_name].value
     print x
 
