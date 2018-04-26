92adcd27970be1e46d6f019cbc906771ebfa1a95
stryker <bcstryker@gmail.com>
2018-04-26 22:13:55 +0000

    lab 6 debugged

--- lab6.py
+++ lab6.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import load_workbook
 
-filename = 'home/student/ipwsp/aws/AWS_SEED.xlsx'
+filename = '/home/student/ipwsp/aws/AWS_SEED.xlsx'
 column = 'A'
 row = 3
 
@@ -14,10 +14,10 @@ class AWS_VPC():
         else:
             self.wb = wb
 
-    def get_row(col, row = 1, max_row = 4):
+    def get_row(self, col, row = 1, max_row = 4):
         ws = self.wb.active
-        sheet = wb['Sheet1']
-        cell_name = column + str(row)
+        sheet = self.wb['Sheet1']
+        cell_name = col + str(row)
         x = sheet[cell_name].value
         return x
 
@@ -26,3 +26,6 @@ col_B = AWS_VPC()
 
 contents_A = col_A.get_row('A')
 contents_B = col_B.get_row('B')
+
+print contents_A
+print contents_B
