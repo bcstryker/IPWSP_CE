f892dd4c68ec4adeffeabaa23be4275f97efd7e6
stryker <bcstryker@gmail.com>
2018-04-27 17:08:56 +0000

    lab 7 changes

---
+++ stryker-lab7.py
@@ -0,0 +1,47 @@
+import sys
+from openpyxl import load_workbook
+
+filename = '/home/student/ipwsp/aws/AWS_SEED.xlsx'
+column = 'A'
+row = 3
+
+class AWS_VPC():
+    def __init__(self):
+        try:
+            wb = load_workbook(filename = filename, data_only = True)
+        except IOError:
+            sys.exit('Could not open file')
+        else:
+            self.wb = wb
+
+    def get_row(self, col, row = 1, start_row = 0, max_row = 4):
+        ws = self.wb.active
+        sheet = self.wb['Sheet1']
+        if max_row > sheet.max_row:
+            print 'Max row referenced out of range.'
+            print 'Setting max row to {}'.format(sheet.max_row)
+            max_row = sheet.max_row
+        test_list = []
+        for row in range(start_row, max_row):
+            #the following line is confusing and seems unnecessary
+            for column in '{}'.format(col):
+                cell_name = column + str(row)
+                x = sheet[cell_name].value
+                temp_list.append(x)
+        return temp_list
+
+def main():
+    vpc_console1 = AWS_VPC()
+    vpc_list = vpc_console1.get_row('A', start_row = 1, max_row = 7)
+    pub_subnet_list = vpc_console1.get_row('B', start_row = 1, max_row = 5)
+    priv_subnet_list = vpc_console1.get_row('C', start_row = 1, max_row = 5)
+    route_table_list = vpc_console1.get_row('D', start_row = 1, max_row = 5)
+    region_list = vpc_console1.get_row('E', start_row = 1, max_row = 5)
+    print vpc_list
+    print pub_subnet_list
+    print priv_subnet_list
+    print route_table_list
+    print region_list
+
+if __name__ == '__main__':
+    main()
